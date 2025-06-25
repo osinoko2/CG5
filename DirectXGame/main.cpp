839a35082715b1a7712d4c98b550103aa4bb9dea
osinoko <k023g0114@g.neec.ac.jp>
2025-06-25 16:03:18 +0900

    ポストエフェクトを実現する

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -234,6 +234,14 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 
 		// 描画
 
+		// TransitionBarrierを元に戻し、PixelShaderが扱えるようにする
+		barrier.Type = D3D12_RESOURCE_BARRIER_TYPE_TRANSITION;                      // TranslationBarrierの設定
+		barrier.Flags = D3D12_RESOURCE_BARRIER_FLAG_NONE;                           // フラグはNoneにしておく
+		barrier.Transition.pResource = renderTextureResource;                       // バリアを張る対象のリソース
+		barrier.Transition.StateBefore = D3D12_RESOURCE_STATE_RENDER_TARGET;        // 遷移前
+		barrier.Transition.StateAfter = D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE; // 遷移後
+		commandList->ResourceBarrier(1, &barrier);                                  // バリアを張る
+
 		// 描画開始
 		dxCommon->PreDraw();
 
@@ -258,14 +266,6 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 
 		// 描画終了
 		dxCommon->PostDraw();
-
-		// TransitionBarrierを元に戻し、PixelShaderが扱えるようにする
-		barrier.Type = D3D12_RESOURCE_BARRIER_TYPE_TRANSITION;                      // TranslationBarrierの設定
-		barrier.Flags = D3D12_RESOURCE_BARRIER_FLAG_NONE;                           // フラグはNoneにしておく
-		barrier.Transition.pResource = renderTextureResource;                       // バリアを張る対象のリソース
-		barrier.Transition.StateBefore = D3D12_RESOURCE_STATE_RENDER_TARGET;        // 遷移前
-		barrier.Transition.StateAfter = D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE; // 遷移後
-		commandList->ResourceBarrier(1, &barrier);                                  // バリアを張る
 	}
 
 	// 解放
