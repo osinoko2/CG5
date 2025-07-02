81fd9dddcc248b8fae0e03abc4239bc35ce35056
osinoko <k023g0114@g.neec.ac.jp>
2025-07-02 14:25:52 +0900

    Vignetting

--- DirectXGame/Resources/shaders/TestPS.hlsl
+++ DirectXGame/Resources/shaders/TestPS.hlsl
@@ -12,6 +12,16 @@ PixelShaderOutput main(VertexShaderOutput input){
     float32_t2 uv = input.texcoord;
     float32_t4 textureColor = gTexture.Sample(gSampler, uv);
     
+    // Vignetting
+    // 周囲を0に、中心になるほど明るくなるように計算で調整
+    float32_t2 correct = input.texcoord * (1.0f - input.texcoord.yx);
+    // correctだけで計算すると中心の最大値が0.0625で暗すぎるのでScaleで調整。
+    float vignette = correct.x * correct.y * 16.0f;
+    // とりあえず0.8乗でそれっぽくしてみた
+    vignette = saturate(pow(vignette, 0.8f));
+    // 係数として乗算
+    textureColor.rgb *= vignette;
+    
     // grayscale
     float32_t value = dot(textureColor.rgb, float32_t3(0.2125f, 0.7154f, 0.0721f));
     output.color = float32_t4(value, value, value, textureColor.a);
