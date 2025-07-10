1739abf934bda4ba3adc396798584910bf039f44
osinoko <k023g0114@g.neec.ac.jp>
2025-07-10 13:59:18 +0900

    gMaterialが分からん

---
+++ DirectXGame/Resources/shaders/DepthBasedOutline.PS.hlsl
@@ -0,0 +1,69 @@
+#include "Test.hlsli"
+
+static const float32_t2 kIndex3x3[3][3] =
+{
+    { { -1.0f, -1.0f }, { 0.0f, -1.0f }, { 1.0f, -1.0f } },
+    { { -1.0f, 0.0f }, { 0.0f, 0.0f }, { 1.0f, 0.0f } },
+    { { -1.0f, 1.0f }, { 0.0f, 1.0f }, { 1.0f, 1.0f } },
+};
+
+static const float32_t kPrewittHorizontalKernel[3][3] =
+{
+    { -1.0f / 6.0f, 0.0f, 1.0f / 6.0f },
+    { -1.0f / 6.0f, 0.0f, 1.0f / 6.0f },
+    { -1.0f / 6.0f, 0.0f, 1.0f / 6.0f },
+};
+
+static const float32_t kPrewittVerticalKernel[3][3] =
+{
+    { -1.0f / 6.0f, -1.0f / 6.0f, -1.0f / 6.0f },
+    { 0.0f, 0.0f, 0.0f },
+    { 1.0f / 6.0f, 1.0f / 6.0f, 1.0f / 6.0f },
+};
+
+float32_t Luminance(float32_t3 v)
+{
+    return dot(v, float32_t3(0.2125f, 0.7154f, 0.0721f));
+}
+
+Texture2D<float32_t4> gTexture : register(t0);
+Texture2D<float32_t> gDepthTexture : register(t1);
+//gMaterial : register(t2);
+SamplerState gSampler : register(s0);
+SamplerState gSamplerPoint : register(s1);
+
+struct PixelShaderOutput
+{
+    float32_t4 color : SV_TARGET0;
+};
+
+PixelShaderOutput main(VertexShaderOutput input)
+{
+    uint32_t width, height;
+    gTexture.GetDimensions(width, height);
+    float32_t2 uvStepSize = float32_t2(rcp(width), rcp(height));
+    float32_t2 difference = float32_t2(0.0f, 0.0f); // 縦横それぞれの畳み込みの結果を格納する
+    // 色を輝度に変換して、畳み込みを行っていく。微分Filter用のkernelになっているので、やること自体は今までと同じ
+    for (int32_t x = 0; x < 3; ++x)
+    {
+        for (int32_t y = 0; y < 3; ++y)
+        {
+            float32_t2 texcoord = input.texcoord + kIndex3x3[x][y] * uvStepSize;
+            float32_t ndcDepth = gDepthTexture.Sample(gSamplerPoint, texcoord);
+            // gMaterial.projectionInverseはCBufferを使って渡しておくこと
+            float32_t4 viewSpace = mul(float32_t4(0.0f, 0.0f, ndcDepth, 1.0f), gMaterial.projectionInverse);
+            float32_t viewZ = viewSpace.z * rcp(viewSpace.w);
+            difference.x += viewZ * kPrewittHorizontalKernel[x][y];
+            difference.y += viewZ * kPrewittVerticalKernel[x][y];
+        }
+    }
+    // 変化の長さをウェイトとして合成。
+    float32_t weight = length(difference);
+    weight = saturate(weight);
+    
+    PixelShaderOutput output;
+    output.color.rgb = (1.0f - weight) * gTexture.Sample(gSampler, input.texcoord).rgb;
+    output.color.a = 1.0f;
+    
+	return output;
+}
--- DirectXGame/Resources/shaders/Vignette.PS.hlsl
+++ DirectXGame/Resources/shaders/Vignette.PS.hlsl
@@ -1,19 +1,17 @@
 #include "Test.hlsli"
 
-Texture2D<float32_t4> gTexture : register(t0); // SRV     register => t
-Texture2D<float32_t4> gMaskTexture : register(t1); // SRV     register => t
-SamplerState gSampler : register(s0);          // Sampler register => s
+Texture2D<float32_t4> gTexture : register(t0);
+SamplerState gSampler : register(s0);
 
-struct PixelShaderOutput{
+struct PixelShaderOutput
+{
     float32_t4 color : SV_TARGET0;
 };
 
 PixelShaderOutput main(VertexShaderOutput input){
     PixelShaderOutput output;
-    float32_t2 uv = input.texcoord;
-    float32_t4 textureColor = gTexture.Sample(gSampler, uv);
-       
-    // Vignetting
+    output.color = gTexture.Sample(gSampler, input.texcoord);
+    
     // 周囲を0に、中心になるほど明るくなるように計算で調整
     float32_t2 correct = input.texcoord * (1.0f - input.texcoord.yx);
     // correctだけで計算すると中心の最大値が0.0625で暗すぎるのでScaleで調整。
@@ -21,10 +19,7 @@ PixelShaderOutput main(VertexShaderOutput input){
     // とりあえず0.8乗でそれっぽくしてみた
     vignette = saturate(pow(vignette, 0.8f));
     // 係数として乗算
-    textureColor.rgb *= vignette;
+    output.color.rgb *= vignette;
     
-    // grayscale
-    float32_t value = dot(textureColor.rgb, float32_t3(0.2125f, 0.7154f, 0.0721f));
-    output.color = float32_t4(value, value, value, textureColor.a);
-    return output;
+	return output;
 }
